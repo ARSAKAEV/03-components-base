3ae72e5e864c697e0d66b9e653768022a3e7bbbe
ARSAKAEV <arsakaev999555@gmail.com>
2022-08-25 08:13:05 +0300

    home-work

---
+++ src/Base.js
@@ -0,0 +1,11 @@
+import img from './redux.jpg';
+function Base(){
+    return(
+        <>
+            <h1>Hello, world</h1>
+            <img src={img}/>
+        </>
+    )
+};
+
+export default Base;
